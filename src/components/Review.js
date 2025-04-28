3e7b17a3350951feae0440d6ded3e7c2fdbfdea7
souravgreatest <jainsourav220@gmail.com>
2025-04-28 15:19:57 +0530

    Completed assignment

--- src/components/Review.js
+++ src/components/Review.js
@@ -3,24 +3,39 @@ import React, { useState } from 'react';
 const reviews = [
   {
     id: 1,
-    name: 'John Doe',
-    job: 'Software Developer',
-    text: 'This is a fantastic product! I highly recommend it to everyone.',
-    image: 'https://randomuser.me/api/portraits/men/1.jpg',
+    name: 'susan smith',
+    job: 'web developer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
     id: 2,
-    name: 'Jane Smith',
-    job: 'Designer',
-    text: 'I absolutely love using this! It has made my work so much easier.',
-    image: 'https://randomuser.me/api/portraits/women/1.jpg',
+    name: 'anna johnson',
+    job: 'web designer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    text:
+      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
   },
   {
     id: 3,
-    name: 'David Johnson',
-    job: 'Product Manager',
-    text: 'An amazing experience, truly a game changer in the industry.',
-    image: 'https://randomuser.me/api/portraits/men/2.jpg',
+    name: 'peter jones',
+    job: 'intern',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    text:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
 ];
 
@@ -46,15 +61,26 @@ function Review() {
   const { name, job, text, image, id } = reviews[index];
 
   return (
-    <div className="review">
+    <div className="review" style={{ maxWidth: '400px', margin: '0 auto', textAlign: 'center' }}>
+      <img
+        className="person-img"
+        src={image}
+        alt={name}
+        style={{
+          width: '150px',
+          height: '150px',
+          borderRadius: '50%',
+          objectFit: 'cover',
+          marginBottom: '1rem',
+        }}
+      />
       <h2 id={`author-${id}`} className="author">{name}</h2>
-      <p className="job">{job}</p>
-      <p className="info">{text}</p>
-      <img className="person-img" src={image} alt={name} />
-      <div className="button-container">
-        <button className="prev-btn" onClick={prevReview}>Prev</button>
-        <button className="next-btn" onClick={nextReview}>Next</button>
-        <button className="random-btn" onClick={randomReview}>Surprise Me</button>
+      <p className="job" style={{ color: 'gray', fontStyle: 'italic' }}>{job}</p>
+      <p className="info" style={{ padding: '0 1rem' }}>{text}</p>
+      <div className="button-container" style={{ marginTop: '1rem' }}>
+        <button className="prev-btn" onClick={prevReview} style={{ margin: '0 5px' }}>Prev</button>
+        <button className="next-btn" onClick={nextReview} style={{ margin: '0 5px' }}>Next</button>
+        <button className="random-btn" onClick={randomReview} style={{  display: 'block' }}>surprise me</button>
       </div>
     </div>
   );
